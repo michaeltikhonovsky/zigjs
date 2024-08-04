efa0132b61376ba22477b95d657d480a8e9ecaba
Noah Solomon <noahsolomon2003@gmail.com>
2024-08-04 17:38:33 -0400

    simplify build and link static c lib

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 
-const math = @cImport({
-    @cInclude("math.h");
-});
+extern fn add(a: i32, b: i32) i32;
+
+extern fn increment(a: i32) i32;
 
 pub fn main() !void {
     const a = 1;
     const b = 2;
-    const c = math.add(a, b);
-    std.debug.print("{d}", .{c});
-    std.debug.print("{d}", .{math.increment(c)});
+    const c = add(a, b);
+    std.debug.print("{d}\n", .{c});
+    std.debug.print("{d}\n", .{increment(c)});
 }
--- src/math.c
+++ src/math.c
@@ -1,9 +1,5 @@
 #include "math.h"
 
-int add(int a, int b){
-  return a + b;
-}
+int add(int a, int b) { return a + b; }
 
-int increment(int a){
-  return a + INCREMENT_BY;
-}
+int increment(int a) { return a + INCREMENT_BY; }
