e14ccaacdcaf77758020e7394194961cfaacbbbd
Michael Tikhonovsky <michaeltikhonovsky@gmail.com>
2024-08-20 00:05:20 -0400

    remove c files and bindings file

--- src/bindings.zig
+++
@@ -1,4 +0,0 @@
-pub const math = struct {
-    pub extern fn add(a: i32, b: i32) i32;
-    pub extern fn increment(a: i32) i32;
-};
--- src/c/math.c
+++
@@ -1,5 +0,0 @@
-#include "math.h"
-
-int add(int a, int b) { return a + b; }
-
-int increment(int a) { return a + INCREMENT_BY; }
